545dfbf0bd5fec8b1c6e1524e8473901c626e20a
jaskaranbedi <jaskaran.deep2@gmail.com>
2020-07-04 21:42:04 +0530

    Updates

---
+++ precache-manifest.408a4dd753f158f7c9d5b28560f23054.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab552c01ef3336dc840cefe51968c97",
+    "url": "/Monsters-Rolodex-React/index.html"
+  },
+  {
+    "revision": "1e3ed8f64eecb549cb6d",
+    "url": "/Monsters-Rolodex-React/static/css/main.34f3f032.chunk.css"
+  },
+  {
+    "revision": "e1c35d727ac6014e12b0",
+    "url": "/Monsters-Rolodex-React/static/js/2.feb6b8d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex-React/static/js/2.feb6b8d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e3ed8f64eecb549cb6d",
+    "url": "/Monsters-Rolodex-React/static/js/main.4e2b82d7.chunk.js"
+  },
+  {
+    "revision": "75abe55bd0b71272b12b",
+    "url": "/Monsters-Rolodex-React/static/js/runtime-main.69ff7294.js"
+  }
+]);
